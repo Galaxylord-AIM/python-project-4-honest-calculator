1975dcd1951467613df05c7712ce5187434a73b0
Ayman Momin <63975056+Galaxylord-AIM@users.noreply.github.com>
2021-12-07 20:34:36 +0530

    Stage 2/5: First calculations

--- honest_calc.py
+++ honest_calc.py
@@ -2,6 +2,8 @@
 msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+msg_3 = "Yeah... division by zero. Smart move..."
+result = 0
 
 while True:
     print(msg_0)
@@ -9,8 +11,24 @@ while True:
     calc = calc.split(" ")
     try:
         x, oper, y = float(calc[0]), calc[1], float(calc[2])
-        if (oper == "+") or (oper == "-") or (oper == "/") or (oper == "*"):
+        if oper == "+":
+            result = x + y
             break
-        print(msg_2)
+        elif oper == "-":
+            result = x - y
+            break
+        elif oper == "/":
+            if (oper == "/") and (y == 0):
+                print(msg_3)
+                continue
+            result = x / y
+            break
+        elif oper == "*":
+            result = x * y
+            break
+        else:
+            print(msg_2)
     except:
         print(msg_1)
+
+print(result)
